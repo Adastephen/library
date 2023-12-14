65635f8033e215603a8a02e11ba05a63072058af
Adastephen <adakolestephen09@gmail.com>
2023-12-14 20:34:22 +0100

    commit

--- library.js
+++ library.js
@@ -5,6 +5,12 @@ let clear = document.querySelector('#clear');
 let output = document.querySelector('#output');
 let myBook;
 
+// let titstatus = document.querySelector('#titlesStatus');
+// let noa = document.queryselector('#nameOfAuthorStatus');
+// let nop = document.queryselector('#numberOfPagesStatus');
+// let nrs = document.queryselector('#numberReadStatus');
+
+
 // class object to control the values
 class Book {
     constructor (titles, nameOfAuthor, noOfPages, noRead) {
@@ -21,9 +27,38 @@ function addBook(bookList) {
     bookList.forEach(book => {
         Object.keys(book).forEach(book => {
             console.log(key, book[key]);
+
         })
     });
 }
+
+
+function listdiv (){
+    let list = document.createElement('div');
+    list.style.cssText = 'width: 80%; margin: auto; background-color: white padding: 30px display: flex; justify-content: space-between;'
+    let ts = document.createElement('p')
+    ts.textContent = 'the title';
+    let ta = document.createElement('p');
+    ta.textContent = 'authors name';
+    let tnp = document.createElement('p');
+    tnp.textContent = 456;
+    let tnr = document.createElement('p');
+    tnr.textContent = 334;
+
+    list.appendChild(ts);
+    list.appendChild(ta);
+    list.appendChild(tnp);
+    list.appendChild(tnr);
+}
+
+
+
+
+// function newDiv () {
+//     let div = document.createElement('div');
+//     div = output;
+//     div.textContent = titstatus; 
+// }
 
 // button that will accept all the values
 submit.addEventListener('click', () => {
@@ -39,16 +74,29 @@ submit.addEventListener('click', () => {
     console.log(myBook);
     addBook(myBook);
     console.log(book1.titles);
+    listdiv();
+    // let addDiv = new newDiv;
+    // addDiv.myBook;
 
+
+
+    // console.log(addDiv);
+    // let cloneOutput = output.cloneNode(true);
+    // return cloneOutput
+    // document.querySelector('#titlesStatus').textContent = myBook.forEach(book => {
+    //     console.log(cloneOutput);
+})
     // display it on the output
-    document.querySelector('#titleStatus').innerHTML = book1.titles;
-    let AuthorStatus = document.queryselector ('#nameOfAuthorStatus');
-    let PageStatus = document.queryselector ('#nameOfPagesStatus');
-    let noOfread = document.queryselector ('#numberReadStatus');
-});
+   
 
 
+   
 
+
+// itemsArray.forEach(function(item) {
+//     var div = document.createElement('div');
+//     div.textContent = item.trim();  // Trim removes leading/trailing spaces
+//     addBook.appendChild(div);
 
 
 
